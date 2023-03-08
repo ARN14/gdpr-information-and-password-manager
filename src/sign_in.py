922389dfac7401d834588ad5e03d17455c30bf6e
ARN14 <ar14@outlook.com>
2023-03-08 14:02:06 +0300

    Fixed writing to database on sign in

--- src/sign_in.py
+++ src/sign_in.py
@@ -1,26 +1,34 @@
-from mock_database import data
 from gdpr_mask import mask
 from create_new_user import create_new_user
+import json
 
 attempts = 1
+
+with open("mock_users_database.json", "r") as file:
+    data = json.loads(file.read())
 
 
 def sign_in():
     username = input("Username:  ")
+
     for index, user in enumerate(data):
         if username == user['username']:
             password = hash(user['password'])
             result = check_password(password)
 
-            if result:
-                info_to_censor = input("What would you like to censor? (space separated for multiple fields)  ")
-                print("Retrieving user information...")
+            if not result:
+                return
 
-                @mask(*info_to_censor.split())
-                def get_client_details():
-                    return data[index]
+            info_to_censor = input("What would you like to censor? (space separated for multiple fields)  ")
+            print("Retrieving user information...")
 
-                print(get_client_details())
+            @mask(*info_to_censor.split())
+            def get_client_details():
+                return data[index]
+
+            print(get_client_details())
+            return
+
         else:
             new_user = input("A user with that username does not exist, would you like to create a new user with that username? (Yes/No)  ")
             if new_user.lower() == "yes":
