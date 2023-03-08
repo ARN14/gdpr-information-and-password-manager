a645dcfc871f7d542c59f9f1949b0b1e3f27c08c
ARN14 <ar14@outlook.com>
2023-03-08 14:01:00 +0300

    Implement function to create new user

--- src/create_new_user.py
+++ src/create_new_user.py
@@ -1,4 +1,23 @@
+import json
+
+
 def create_new_user(username):
     password = input("Create a new password:  ")
-    with open("./mock_new_user_database") as file:
-        print(file.read())
+    about_me = input("About me:  ")
+
+    with open("mock_new_users_database.json", "r") as file:
+        current_users = json.loads(file.read())
+        new_user = {
+            "username": username,
+            "passsword": password,
+            "about_me": about_me
+        }
+
+        print(f"User is being created... {new_user}")
+
+        current_users.append(new_user)
+
+        with open("mock_new_users_database.json", "w") as file:
+            json.dump(current_users, file, indent=2)
+
+        print("User created successfully")
--- test/test_create_new_user.py
+++ test/test_create_new_user.py
@@ -1,12 +1,24 @@
 from src.create_new_user import create_new_user
-from unittest.mock import patch
+from unittest.mock import Mock, patch
+import json
 
-@patch("create_new_user.input", return_value="Password")
+
+def mock_input(*args):
+    return "Password"
+
+
+@patch("src.create_new_user.input", side_effect=mock_input)
 def test_adds_username_and_password_from_input(patch):
+    with open("mock_new_users_database.json", "w") as file:
+        json.dump([], file)
+
     create_new_user("ABC")
 
-    assert "new_users" == [{
-    "username": "ABC",
-    "passsword": "Password"
-    }]
+    with open("mock_new_users_database.json", "r") as file:
+        read_file = json.loads(file.read())
 
+    assert read_file == [{
+    "username": "ABC",
+    "passsword": "Password",
+    "about_me": "Password"
+    }]
